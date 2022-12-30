47c6fb60b56c98aa1c232a2eea63c40cb12522b6
servedbyskull <public.servedbyskull@protonmail.com>
2022-12-30 15:08:57 +0530

    new wrapper

--- field/git.wrapper.sh
+++ field/git.wrapper.sh
@@ -1,137 +1,226 @@
 #!/bin/bash
 
-# Set the URL of the file to check for updates
+# url of the file and wrapper to check for updates
 file_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/src/git.src.sh"
+wrapper_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/git.sh"
 
+# directory to store the files
 store_dir="$HOME/.sbs"
 
-# Create the directory if it doesn't exist
+# file paths
+file_path="$store_dir/$(basename "$file_url")"
+wrapper_path="$(basename "$wrapper_url")"
+
+file_last_update_file="$store_dir/lu"
+wrapper_last_update_file="$store_dir/luw"
+
+file_temp_path="$store_dir/$(basename "$file_url").t"
+wrapper_temp_path="$store_dir/$(basename "$wrapper_url").t"
+
+# options
+force_file_update=false
+update_wrapper=false
+
+# set the variables
+network_connection=false
+file_exists_on_url=false
+wrapper_exists_on_url=false
+
+file_exists_in_store=false
+
+file_older_then_1_day=false
+
+# check options
+for arg in "$@"; do
+    case "$arg" in
+    -f | --force)
+        force_file_update=true
+        ;;
+    -u | --update)
+        update_wrapper=true
+        ;;
+    esac
+done
+
+# create the directory if it doesn't exist
 if [ ! -d "$store_dir" ]; then
     mkdir "$store_dir"
+else
+    # check if the file exists in the store
+    if [ -f "$file_path" ]; then
+        file_exists_in_store=true
+    fi
+
+    # check both last update file exists
+    if [ ! -f "$file_last_update_file" ]; then
+        touch "$file_last_update_file"
+        echo 0 >"$file_last_update_file"
+    else
+        # check if the file is older then 1 day
+        if [ "$(($(date +%s) - $(date -r "$file_last_update_file" +%s)))" -gt 86400 ]; then
+            file_older_then_1_day=true
+        fi
+    fi
+
+    if [ ! -f "$wrapper_last_update_file" ]; then
+        touch "$wrapper_last_update_file"
+        echo 0 >"$wrapper_last_update_file"
+    fi
 fi
 
-# Set the path to the file to store the latest version, take the name from the file URL
-file_path="$store_dir/$(basename "$file_url")"
-last_update_file="$store_dir/lu"
-
-# Check if the last update file exists
-if [ ! -f "$last_update_file" ]; then
-    # Create the file
-    touch "$last_update_file"
-
-    # Set the last update time to 0
-    echo 0 >"$last_update_file"
-fi
-
-network_connection=false
-
+# check if the network is connected
 if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
     network_connection=true
 fi
 
-file_exists_on_url=false
+# check if the both file & wrapper exists on the url if the network is connected
+if [ "$network_connection" = true ]; then
+    if wget -q --spider "$file_url"; then
+        file_exists_on_url=true
+    fi
 
-if wget -q --spider "$file_url"; then
-    file_exists_on_url=true
+    if wget -q --spider "$wrapper_url"; then
+        wrapper_exists_on_url=true
+    fi
 fi
 
-force_update=false
+function update_wrapper() {
 
-# -f or --force to force update
-if [ "$1" = "-f" ] || [ "$1" = "--force" ]; then
-    force_update=true
-fi
+    # check if the wrapper exists on the url
+    if [ "$wrapper_exists_on_url" = true ]; then
+        # download the wrapper
+        wget -q "$wrapper_url" -O "$wrapper_temp_path"
 
-update_self=false
+        # check if the wrapper is downloaded
+        if [ -f "$wrapper_temp_path" ]; then
+            # check if the wrapper is different
+            if ! cmp -s "$wrapper_temp_path" "$wrapper_path"; then
+                # replace the wrapper
+                mv "$wrapper_temp_path" "$wrapper_path"
 
-# -u or --update to update the wrapper
-if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then
-    update_self=true
-fi
+                # set the last update time
+                echo "$(date +%s)" >"$wrapper_last_update_file"
 
-# if update self is true, update the wrapper else check if the file exists
-wrapper_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/git.sh"
-# wrapper will always be in current directory
-# only while checking for updates, the temporary file will be in the store directory
-wrapper_path="$(basename "$wrapper_url")"
-wrapper_temp_path="$store_dir/$(basename "$wrapper_url").t"
-wrapper_last_update_file="$store_dir/luw"
+                # in green
+                echo -e "\e[32mWrapper updated\e[0m"
+            else
+                # in yellow
+                echo -e "\e[33mWrapper is up to date\e[0m"
+            fi
 
-if [ ! -f "$wrapper_last_update_file" ]; then
-    touch "$wrapper_last_update_file"
-    echo 0 >"$wrapper_last_update_file"
-fi
-
-# after updating the wrapper, dont run the script just exit and prin the message in green
-if [ "$update_self" = true ]; then
-    if [ "$network_connection" = true ]; then
-        if [ "$file_exists_on_url" = true ]; then
-            echo "" && echo -e "\e[32mDownloading wrapper...\e[0m" && sleep 1
-            wget -q "$wrapper_url" -O "$wrapper_temp_path"
-            cmp -s "$wrapper_path" "$wrapper_temp_path" || {rm "$wrapper_path"
-            mv "$wrapper_temp_path" "$wrapper_path"
-            date +%s >"$wrapper_last_update_file"
-            echo -e "\e[32mWrapper updated successfully\e[0m"}
+            # remove the temp file
             rm "$wrapper_temp_path"
-            exit 0
         else
-            echo -e "\e[31mFile not found on URL\e[0m"
-            exit 1
+            # in red
+            echo -e "\e[31mWrapper download failed\e[0m"
         fi
     else
-        echo -e "\e[31mNo network connection\e[0m"
-        exit 1
+        # in red
+        echo -e "\e[31mWrapper doesn't exist on the url\e[0m"
     fi
+}
+
+function update_file() {
+
+    # check if the file exists on the url
+    if [ "$file_exists_on_url" = true ]; then
+        # download the file
+        wget -q "$file_url" -O "$file_temp_path"
+
+        # check if the file is downloaded
+        if [ -f "$file_temp_path" ]; then
+            # check if the file is different
+            if ! cmp -s "$file_temp_path" "$file_path"; then
+                # replace the file
+                mv "$file_temp_path" "$file_path"
+
+                # set the last update time
+                echo "$(date +%s)" >"$file_last_update_file"
+
+                # in green
+                echo -e "\e[32mFile updated\e[0m"
+            else
+                # in yellow
+                echo -e "\e[33mFile is up to date\e[0m"
+            fi
+
+            # remove the temp file
+            rm "$file_temp_path"
+        else
+            # in red
+            echo -e "\e[31mFile download failed\e[0m"
+        fi
+    else
+        # in red
+        echo -e "\e[31mFile doesn't exist on the url\e[0m"
+    fi
+}
+
+function download_file() {
+
+    # check if the file exists on the url
+    if [ "$file_exists_on_url" = true ]; then
+        # download the file
+        wget -q "$file_url" -O "$file_path"
+
+        # check if the file is downloaded
+        if [ -f "$file_path" ]; then
+            # set the last update time
+            echo "$(date +%s)" >"$file_last_update_file"
+
+            # in green
+            echo -e "\e[32mFile downloaded\e[0m"
+        else
+            # in red
+            echo -e "\e[31mFile download failed\e[0m"
+        fi
+    else
+        # in red
+        echo -e "\e[31mFile doesn't exist on the url\e[0m"
+    fi
+}
+
+# if update wrapper is true
+if [ "$update_wrapper" = true ]; then
+    update_wrapper
+
+    # exit the script
+    exit
 fi
 
-if [ -f "$file_path" ]; then
-    # check for update is true, update the file else check file older than 1 day
-    if [ "$force_update" = true ] || [ "$(($(date +%s) - $(cat "$last_update_file")))" -gt 86400 ]; then
-        if [ "$network_connection" = true ]; then
-            if [ "$file_exists_on_url" = true ]; then
-                echo "" && echo -e "\e[32mDownloading file...\e[0m" && sleep 1
-                wget -q "$file_url" -O "$file_path.t"
-                cmp -s "$file_path" "$file_path.t" || {rm "$file_path"
-                mv "$file_path.t" "$file_path"
-                date +%s >"$last_update_file"
-                bash "$file_path"}
-                rm "$file_path.t"
-                bash "$file_path"
-            else
-                bash "$file_path"
-            fi
-        else
-            bash "$file_path"
+# if force file update is true
+if [ "$force_file_update" = true ]; then
+    update_file
+
+    # run the file
+    bash "$file_path"
+fi
+
+# if both update wrapper & force file update is true, update the wrapper & run the file
+if [ "$update_wrapper" = true ] && [ "$force_file_update" = true ]; then
+    update_wrapper
+    update_file
+
+    # run the file
+    bash "$file_path"
+fi
+
+# if both false
+if [ "$update_wrapper" = false ] && [ "$force_file_update" = false ]; then
+    # check file exists in store
+    if [ "$file_exists_in_store" = true ]; then
+        # check if the file is older then 1 day
+        if [ "$file_older_then_1_day" = true ]; then
+            update_file
         fi
-    else
+
+        # run the file
         bash "$file_path"
-    fi
-else
-    if [ "$network_connection" = true ]; then
-        if [ "$file_exists_on_url" = true ]; then
-
-            # file url in red
-            echo -e "\e[31mFile URL: $file_url\e[0m"
-            echo ""
-
-            # ask for double confirmation in red
-            read -p $'\e[31mAre you sure you want to download this file? [ check the file url above ] [y/N]\e[0m ' -n 1 -r </dev/tty && echo && [[ $REPLY =~ ^[Yy]$ ]] || exit 1 && echo "" && echo -e "\e[32mDownloading file...\e[0m" && sleep 1
-
-            wget -q "$file_url" -O "$file_path"
-            date +%s >"$last_update_file"
-            bash "$file_path"
-        else
-            if [ -f "src/$(basename "$file_url")" ]; then
-                bash "src/$(basename "$file_url")"
-            else
-                echo -e "\e[31mNo internet connection\e[0m"
-            fi
-        fi
     else
-        if [ -f "src/$(basename "$file_url")" ]; then
-            bash "src/$(basename "$file_url")"
-        else
-            echo -e "\e[31mNo internet connection\e[0m"
-        fi
+        # download the file
+        download_file
+
+        # run the file
+        bash "$file_path"
     fi
 fi
--- git.sh
+++ git.sh
@@ -1,113 +1,226 @@
 #!/bin/bash
 
-a="https://raw.githubusercontent.com/servedbyskull/scripts/main/src/git.src.sh"
-b="$HOME/.sbs"
-c="$b/$(basename "$a")"
-d="$b/lu"
-z="https://raw.githubusercontent.com/servedbyskull/scripts/main/git.sh"
-y="$b/$(basename "$z")"
-s="$(basename "$z")"
-x="$b/luw"
-e=false
-f=false
-g=false
-k=false
+# url of the file and wrapper to check for updates
+file_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/src/git.src.sh"
+wrapper_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/git.sh"
 
-if [ ! -d "$b" ]; then
-    mkdir "$b"
-fi
+# directory to store the files
+store_dir="$HOME/.sbs"
 
-if [ ! -f "$d" ]; then
-    touch "$d"
-    echo 0 >"$d"
-fi
+# file paths
+file_path="$store_dir/$(basename "$file_url")"
+wrapper_path="$(basename "$wrapper_url")"
 
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-    e=true
-fi
+file_last_update_file="$store_dir/lu"
+wrapper_last_update_file="$store_dir/luw"
 
-if wget -q --spider "$a"; then
-    f=true
-fi
+file_temp_path="$store_dir/$(basename "$file_url").t"
+wrapper_temp_path="$store_dir/$(basename "$wrapper_url").t"
 
-if [ "$1" = "-f" ] || [ "$1" = "--force" ]; then
-    g=true
-fi
+# options
+force_file_update=false
+update_wrapper=false
 
-if [ "$1" = "-u" ] || [ "$1" = "--update" ]; then
-    k=true
-fi
+# set the variables
+network_connection=false
+file_exists_on_url=false
+wrapper_exists_on_url=false
 
-if [ ! -f "$x" ]; then
-    touch "$x"
-    echo 0 >"$x"
-fi
+file_exists_in_store=false
 
-if [ "$k" = true ]; then
-    if [ "$e" = true ]; then
-        if [ "$f" = true ]; then
-            echo "" && echo -e "\e[32mDownloading wrapper...\e[0m" && sleep 1
-            wget -q "$z" -O "$y.t"
-            if ! cmp -s "$y" "$y.t"; then
-                rm "$y"
-                mv "$y.t" "$y"
-                date +%s >"$x"
-            else
-                rm "$y.t"
-            fi
-            bash "$y" -f
-        else
-            echo -e "\e[31mFile not found on URL\e[0m"
-            exit 1
-        fi
-    else
-        echo -e "\e[31mNo network connection\e[0m"
-        exit 1
-    fi
-fi
+file_older_then_1_day=false
 
-if [ -f "$c" ]; then
-    if [ "$g" = true ] || [ "$(($(date +%s) - $(cat "$d")))" -gt 86400 ]; then
-        if [ "$e" = true ]; then
-            if [ "$f" = true ]; then
-                echo "" && echo -e "\e[32mDownloading file...\e[0m" && sleep 1
-                wget -q "$a" -O "$c.t"
-                if ! cmp -s "$c" "$c.t"; then
-                    rm "$c"
-                    mv "$c.t" "$c"
-                    date +%s >"$d"
-                else
-                    rm "$c.t"
-                fi
-                bash "$c"
-            else
-                bash "$c"
-            fi
-        else
-            bash "$c"
-        fi
-    else
-        bash "$c"
-    fi
+# check options
+for arg in "$@"; do
+    case "$arg" in
+    -f | --force)
+        force_file_update=true
+        ;;
+    -u | --update)
+        update_wrapper=true
+        ;;
+    esac
+done
+
+# create the directory if it doesn't exist
+if [ ! -d "$store_dir" ]; then
+    mkdir "$store_dir"
 else
-    if [ "$e" = true ]; then
-        if [ "$f" = true ]; then
-            read -p $'\e[31mAre you sure you want to download this file? [ check the file url above ] [y/N]\e[0m ' -n 1 -r </dev/tty && echo && [[ $REPLY =~ ^[Yy]$ ]] || exit 1 && echo "" && echo -e "\e[32mDownloading file...\e[0m" && sleep 1
-            wget -q "$a" -O "$c"
-            date +%s >"$d"
-            bash "$c"
-        else
-            if [ -f "src/$(basename "$a")" ]; then
-                bash "src/$(basename "$a")"
+    # check if the file exists in the store
+    if [ -f "$file_path" ]; then
+        file_exists_in_store=true
+    fi
+
+    # check both last update file exists
+    if [ ! -f "$file_last_update_file" ]; then
+        touch "$file_last_update_file"
+        echo 0 >"$file_last_update_file"
+    else
+        # check if the file is older then 1 day
+        if [ "$(($(date +%s) - $(date -r "$file_last_update_file" +%s)))" -gt 86400 ]; then
+            file_older_then_1_day=true
+        fi
+    fi
+
+    if [ ! -f "$wrapper_last_update_file" ]; then
+        touch "$wrapper_last_update_file"
+        echo 0 >"$wrapper_last_update_file"
+    fi
+fi
+
+# check if the network is connected
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+    network_connection=true
+fi
+
+# check if the both file & wrapper exists on the url if the network is connected
+if [ "$network_connection" = true ]; then
+    if wget -q --spider "$file_url"; then
+        file_exists_on_url=true
+    fi
+
+    if wget -q --spider "$wrapper_url"; then
+        wrapper_exists_on_url=true
+    fi
+fi
+
+function update_wrapper() {
+
+    # check if the wrapper exists on the url
+    if [ "$wrapper_exists_on_url" = true ]; then
+        # download the wrapper
+        wget -q "$wrapper_url" -O "$wrapper_temp_path"
+
+        # check if the wrapper is downloaded
+        if [ -f "$wrapper_temp_path" ]; then
+            # check if the wrapper is different
+            if ! cmp -s "$wrapper_temp_path" "$wrapper_path"; then
+                # replace the wrapper
+                mv "$wrapper_temp_path" "$wrapper_path"
+
+                # set the last update time
+                echo "$(date +%s)" >"$wrapper_last_update_file"
+
+                # in green
+                echo -e "\e[32mWrapper updated\e[0m"
             else
-                echo -e "\e[31mNo internet connection\e[0m"
+                # in yellow
+                echo -e "\e[33mWrapper is up to date\e[0m"
             fi
+
+            # remove the temp file
+            rm "$wrapper_temp_path"
+        else
+            # in red
+            echo -e "\e[31mWrapper download failed\e[0m"
         fi
     else
-        if [ -f "src/$(basename "$a")" ]; then
-            bash "src/$(basename "$a")"
+        # in red
+        echo -e "\e[31mWrapper doesn't exist on the url\e[0m"
+    fi
+}
+
+function update_file() {
+
+    # check if the file exists on the url
+    if [ "$file_exists_on_url" = true ]; then
+        # download the file
+        wget -q "$file_url" -O "$file_temp_path"
+
+        # check if the file is downloaded
+        if [ -f "$file_temp_path" ]; then
+            # check if the file is different
+            if ! cmp -s "$file_temp_path" "$file_path"; then
+                # replace the file
+                mv "$file_temp_path" "$file_path"
+
+                # set the last update time
+                echo "$(date +%s)" >"$file_last_update_file"
+
+                # in green
+                echo -e "\e[32mFile updated\e[0m"
+            else
+                # in yellow
+                echo -e "\e[33mFile is up to date\e[0m"
+            fi
+
+            # remove the temp file
+            rm "$file_temp_path"
         else
-            echo -e "\e[31mNo internet connection\e[0m"
+            # in red
+            echo -e "\e[31mFile download failed\e[0m"
         fi
+    else
+        # in red
+        echo -e "\e[31mFile doesn't exist on the url\e[0m"
+    fi
+}
+
+function download_file() {
+
+    # check if the file exists on the url
+    if [ "$file_exists_on_url" = true ]; then
+        # download the file
+        wget -q "$file_url" -O "$file_path"
+
+        # check if the file is downloaded
+        if [ -f "$file_path" ]; then
+            # set the last update time
+            echo "$(date +%s)" >"$file_last_update_file"
+
+            # in green
+            echo -e "\e[32mFile downloaded\e[0m"
+        else
+            # in red
+            echo -e "\e[31mFile download failed\e[0m"
+        fi
+    else
+        # in red
+        echo -e "\e[31mFile doesn't exist on the url\e[0m"
+    fi
+}
+
+# if update wrapper is true
+if [ "$update_wrapper" = true ]; then
+    update_wrapper
+
+    # exit the script
+    exit
+fi
+
+# if force file update is true
+if [ "$force_file_update" = true ]; then
+    update_file
+
+    # run the file
+    bash "$file_path"
+fi
+
+# if both update wrapper & force file update is true, update the wrapper & run the file
+if [ "$update_wrapper" = true ] && [ "$force_file_update" = true ]; then
+    update_wrapper
+    update_file
+
+    # run the file
+    bash "$file_path"
+fi
+
+# if both false
+if [ "$update_wrapper" = false ] && [ "$force_file_update" = false ]; then
+    # check file exists in store
+    if [ "$file_exists_in_store" = true ]; then
+        # check if the file is older then 1 day
+        if [ "$file_older_then_1_day" = true ]; then
+            update_file
+        fi
+
+        # run the file
+        bash "$file_path"
+    else
+        # download the file
+        download_file
+
+        # run the file
+        bash "$file_path"
     fi
 fi
