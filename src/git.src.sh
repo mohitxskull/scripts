2f9c2ea040241eef5deeb0733ee4186bbfd78db0
servedbyskull <public.servedbyskull@protonmail.com>
2023-02-05 12:36:13 +0530

    fix

--- src/git.src.sh
+++ src/git.src.sh
@@ -13,19 +13,20 @@ function setupRepo() {
     rm -rf .git
 
     # option to select the branch default name
-    echo ""
-    echo "Select the default branch name (default: main):"
-    echo ""
-    read -r branchname
-    if [[ "$branchname" == "" ]]; then
-        branchname="main"
-    fi
-
-    git init -m "$branchname"
-
+    # echo ""
+    # echo "Select the default branch name (default: main):"
+    # echo ""
+    # read -r branchname
+    # if [[ "$branchname" == "" ]]; then
+    #     branchname="main"
+    # fi
     echo ""
 
-    echo -e "Git repository initialized with \033[0;32m$branchname\033[0m as default branch."
+    git init -b main
+
+    echo ""
+
+    # echo -e "Git repository initialized with \033[0;32m$branchname\033[0m as default branch."
 
     # Set the user's email and name
     echo ""
