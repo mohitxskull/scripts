3dfefe9c01fb19fafa860a8d6ae4589b78ea1c6d
servedbyskull <public.servedbyskull@protonmail.com>
2023-02-05 10:38:01 +0530

    updater self update

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 file_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/src/git.src.sh"
+self_url="https://raw.githubusercontent.com/servedbyskull/scripts/main/src/git.sh"
 store="$HOME/.sbs"
 lu="$store/lu"
 
@@ -13,9 +14,18 @@ if [ ! -f "$lu" ]; then
     echo 0 >"$lu"
 fi
 
+# self update -s flag
+if [ "$1" = "-s" ]; then
+    echo "" && echo -e "\e[32mUpdating self...\e[0m" && echo ""
+    curl -s "$self_url" >"$0"
+    chmod +x "$0"
+    exit
+fi
+
 if [ "$1" = "-u" ] || [ "$(($(date +%s) - $(cat "$lu")))" -gt 86400 ]; then
     echo "" && echo -e "\e[32mUpdating file...\e[0m" && echo ""
-    curl -s "$file_url" >"$store/$(basename "$file_url")"
+    # use -H to avoid caching
+    curl -H "Cache-Control: no-cache" -s "$file_url" >"$store/$(basename "$file_url")"
     echo "$(date +%s)" >"$lu"
 fi
 
