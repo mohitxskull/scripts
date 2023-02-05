1d03b5fa67548adb98aa614a6c5a675f1374ba2c
servedbyskull <public.servedbyskull@protonmail.com>
2023-02-05 14:28:15 +0530

    Update

--- src/git.src.sh
+++ src/git.src.sh
@@ -1,7 +1,56 @@
 #!/bin/bash
 
 AUTHOR="Skull"
-VERSION="2.3"
+VERSION="2.4"
+
+# Function to push changes to the remote
+# Arguments:
+#   - $1: option for pulling changes before pushing (U = update, F = update and force push)
+function pushChanges() {
+
+    clear
+
+    # branch name = current branch name
+    branchname=$(git branch --show-current)
+
+    if [[ "$1" == "U" ]]; then
+        clear
+
+        # Pull the latest changes from the remote
+        echo "Pulling latest changes from remote..."
+        git pull --ff origin "$branchname"
+    fi
+
+    # Add all changed files
+    git add .
+
+    clear
+
+    # Prompt the user for a commit message
+    echo ""
+    echo "Enter your commit message: "
+    read -r commitmsg
+
+    # Create the commit
+    git commit -m "$commitmsg"
+
+    clear
+    echo ""
+
+    # print selected branch name
+    echo "Current branch: $branchname"
+    echo ""
+
+    # Push the changes to the remote
+    if [[ "$2" == "F" ]]; then
+        # Force push to resolve any non-fast-forward conflicts
+        echo "Pushing changes to remote (resolving non-fast-forward conflicts)..."
+        git push -u -f origin "$branchname"
+    else
+        echo "Pushing changes to remote..."
+        git push -u origin "$branchname"
+    fi
+}
 
 # Function to initialize a new Git repository and add a remote
 function setupRepo() {
@@ -56,55 +105,13 @@ function setupRepo() {
     # success in green color
     echo ""
     echo -e "\033[0;32mSuccessfully set up Git repository\033[0m"
-}
-
-# Function to push changes to the remote
-# Arguments:
-#   - $1: option for pulling changes before pushing (U = update, F = update and force push)
-function pushChanges() {
-
-    clear
-
-    # branch name = current branch name
-    branchname=$(git branch --show-current)
-
-    if [[ "$1" == "U" ]]; then
-        clear
-
-        # Pull the latest changes from the remote
-        echo "Pulling latest changes from remote..."
-        git pull --ff origin "$branchname"
-    fi
-
-    # Add all changed files
-    git add .
-
-    clear
-
-    # Prompt the user for a commit message
-    echo ""
-    echo "Enter your commit message: "
-    read -r commitmsg
-
-    # Create the commit
-    git commit -m "$commitmsg"
-
-    clear
-    echo ""
-
-    # print selected branch name
-    echo "Current branch: $branchname"
-    echo ""
 
     # Push the changes to the remote
-    if [[ "$2" == "F" ]]; then
-        # Force push to resolve any non-fast-forward conflicts
-        echo "Pushing changes to remote (resolving non-fast-forward conflicts)..."
-        git push -u -f origin "$branchname"
-    else
-        echo "Pushing changes to remote..."
-        git push -u origin "$branchname"
-    fi
+    pushChanges
+
+    echo ""
+    # print in green color
+    echo -e "\033[0;32mSuccessfully pushed changes to remote\033[0m"
 }
 
 # switch branch function
@@ -124,6 +131,7 @@ function switchBranch() {
         echo "Invalid branch name"
         return 1
     fi
+    echo ""
     git checkout "$branchname"
     echo ""
     echo -e "Switched to \033[0;32m$branchname\033[0m branch"
@@ -141,12 +149,17 @@ echo -e "Author: \033[0;32m$AUTHOR\033[0m"
 echo "-------------------------------"
 echo ""
 
-git fetch --all
-
 # Check if a Git repository has been set up
 if [ -a ".git" ]; then
 
     echo "Git repository already exists."
+
+    echo ""
+    # print in green Syncing with remote
+    echo -e "\033[0;32mSyncing with remote...\033[0m"
+    git fetch --all
+    echo ""
+
     echo "What would you like to do?"
     echo ""
     # print current branch name
@@ -158,20 +171,18 @@ if [ -a ".git" ]; then
     echo ""
     echo "-------------------------------"
     echo "1: Initialize a new repository"
-    echo "2: Push changes to the remote"
-    echo "3: Update and push changes to the remote"
-    echo -e "4: Force push changes to the remote \033[0;33m(Warning: this will overwrite any changes on the remote)\033[0m"
-    echo -e "5: Switch branch"
+    echo "2: Update and push changes to the remote"
+    echo -e "3: Force push changes to the remote \033[0;33m(Warning: this will overwrite any changes on the remote)\033[0m"
+    echo -e "4: Switch branch"
     echo "-------------------------------"
     echo ""
     read -r selectedOption
 
     case "$selectedOption" in
     1) setupRepo ;;
-    2) pushChanges ;;
-    3) pushChanges "U" ;;
-    4) pushChanges "U" "F" ;;
-    5) switchBranch ;;
+    2) pushChanges "U" ;;
+    3) pushChanges "U" "F" ;;
+    4) switchBranch ;;
     *) echo "Invalid option. Please try again." ;;
     esac
 
