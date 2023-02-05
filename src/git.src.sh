29d9b807a2ed304dc834070b7a2fb9a7397c3bd1
servedbyskull <public.servedbyskull@protonmail.com>
2023-02-05 13:10:32 +0530

    fix

--- src/git.src.sh
+++ src/git.src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AUTHOR="Skull"
-VERSION="2.2"
+VERSION="2.3"
 
 # Function to initialize a new Git repository and add a remote
 function setupRepo() {
@@ -23,11 +23,10 @@ function setupRepo() {
     echo ""
 
     git init -b main
-    git checkout -b main
 
     echo ""
 
-    # echo -e "Git repository initialized with \033[0;32m$branchname\033[0m as default branch."
+    echo -e "Git repository initialized with \033[0;32mmain\033[0m branch"
 
     # Set the user's email and name
     echo ""
@@ -63,30 +62,24 @@ function setupRepo() {
 # Arguments:
 #   - $1: option for pulling changes before pushing (U = update, F = update and force push)
 function pushChanges() {
-    mapfile -t branches < <(git branch -r | grep -v HEAD | sed 's/origin\///')
 
-    # give the user the option to select a branch if more than one exists
-    if [ ${#branches[@]} -gt 1 ]; then
-        echo "Select a branch to push to:"
-        echo ""
-        for i in "${!branches[@]}"; do
-            echo "$((i + 1)): ${branches[$i]}"
-        done
-        echo ""
-        read -r selectedBranch
-    else
-        selectedBranch=1
+    clear
+
+    # give option to type the branch name, if not main will be used
+    echo ""
+    echo "Select the branch name (default: main):"
+    echo ""
+    read -r branchname
+    if [[ "$branchname" == "" ]]; then
+        branchname="main"
     fi
-
-    # trim spaces around the selected branch
-    branches[selectedBranch - 1]="${branches[$((selectedBranch - 1))]// /}"
 
     if [[ "$1" == "U" ]]; then
         clear
 
         # Pull the latest changes from the remote
         echo "Pulling latest changes from remote..."
-        git pull --ff origin "${branches[$((selectedBranch - 1))]}"
+        git pull --ff origin "$branchname"
     fi
 
     # Add all changed files
@@ -106,60 +99,18 @@ function pushChanges() {
     echo ""
 
     # print selected branch name
-    echo "Selected branch: ${branches[$((selectedBranch - 1))]}"
+    echo "Selected branch: $branchname"
     echo ""
 
     # Push the changes to the remote
     if [[ "$2" == "F" ]]; then
         # Force push to resolve any non-fast-forward conflicts
         echo "Pushing changes to remote (resolving non-fast-forward conflicts)..."
-        git push -u -f origin "${branches[$((selectedBranch - 1))]}"
+        git push -u -f origin "$branchname"
     else
         echo "Pushing changes to remote..."
-        git push -u origin "${branches[$((selectedBranch - 1))]}"
+        git push -u origin "$branchname"
     fi
-}
-
-# Function to add a new branch
-# Arguments:
-#   - $1: option for switching to the new branch (S = switch)
-function addBranch() {
-    # Prompt the user for a branch name
-    echo ""
-    echo "Enter the branch name: "
-    read -r branchname
-
-    # Create the branch
-    git checkout -b "$branchname"
-
-    if [[ "$1" == "S" ]]; then
-        # Switch to the new branch
-        git checkout "$branchname"
-    fi
-}
-
-# Function to switch to a branch
-function switchBranch() {
-    mapfile -t branches < <(git branch -r | grep -v HEAD | sed 's/origin\///')
-
-    # give the user the option to select a branch if more than one exists
-    if [ ${#branches[@]} -gt 1 ]; then
-        echo "Select a branch to switch to:"
-        echo ""
-        for i in "${!branches[@]}"; do
-            echo "$((i + 1)): ${branches[$i]}"
-        done
-        echo ""
-        read -r selectedBranch
-    else
-        selectedBranch=1
-    fi
-
-    # trim spaces around the selected branch
-    branches[selectedBranch - 1]="${branches[$((selectedBranch - 1))]// /}"
-
-    # Switch to the selected branch
-    git checkout "${branches[$((selectedBranch - 1))]}"
 }
 
 echo ""
@@ -182,18 +133,11 @@ if [ -a ".git" ]; then
     # print current branch name
     echo -e "Current branch: \033[0;32m$(git branch --show-current)\033[0m"
     echo ""
-    # print all branches, trim spaces and remove the "origin/" prefix
-    echo "All branches:"
-    echo -e "\033[0;32m$(git branch -r | grep -v HEAD | sed 's/origin\///' | sed 's/ //g')\033[0m"
-    echo ""
     echo "-------------------------------"
     echo "1: Initialize a new repository"
     echo "2: Push changes to the remote"
     echo "3: Update and push changes to the remote"
     echo -e "4: Force push changes to the remote \033[0;33m(Warning: this will overwrite any changes on the remote)\033[0m"
-    # note in green that "55: add branch and switch to it"
-    echo -e "5: Add branch \033[0;32m(55: add branch and switch to it)\033[0m"
-    echo "6: Switch branch"
     echo "-------------------------------"
     echo ""
     read -r selectedOption
@@ -203,9 +147,6 @@ if [ -a ".git" ]; then
     2) pushChanges ;;
     3) pushChanges "U" ;;
     4) pushChanges "U" "F" ;;
-    5) addBranch ;;
-    55) addBranch "S" ;;
-    6) switchBranch ;;
     *) echo "Invalid option. Please try again." ;;
     esac
 
