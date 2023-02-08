db6fbe52e96ab8805eca1b06a065a46561b55b37
servedbyskull <public.servedbyskull@protonmail.com>
2023-02-08 17:01:34 +0530

    remove empty git src file

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.8"
+VERSION="0.9"
 
 echo ""
 echo -e "\e[32mGit script v$VERSION\e[0m"
@@ -9,6 +9,11 @@ file_url="https://raw.githubusercontent.com/mohitxskull/scripts/main/src/git.src
 self_url="https://raw.githubusercontent.com/mohitxskull/scripts/main/git.sh"
 store="$HOME/.sbs"
 lu="$store/lu"
+
+# if file is Empty, delete it
+if [ -f "$store/$(basename "$file_url")" ] && [ ! -s "$store/$(basename "$file_url")" ]; then
+    rm "$store/$(basename "$file_url")"
+fi
 
 if [ ! -d "$store" ]; then
     mkdir "$store"
@@ -30,7 +35,7 @@ fi
 # -u flag to update file
 if [ ! -f "$store/$(basename "$file_url")" ] || [ "$(date +%s)" -gt "$(($(cat "$lu") + 86400))" ] || [ "$1" = "-u" ]; then
     echo "" && echo -e "\e[32mUpdating file...\e[0m" && echo ""
-    # use -H to avoid caching
+    # use -H to avoid caching, add max-time to avoid hanging
     curl -H "Cache-Control: no-cache" -s "$file_url" >"$store/$(basename "$file_url")"
     echo "$(date +%s)" >"$lu"
 fi
